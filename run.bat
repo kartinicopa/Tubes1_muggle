f1044a3a8e25a25239ed081d0a5bf62f3d513bfa
Muhammad Raihan Iqbal <raihanvega24@gmail.com>
2023-02-06 09:35:44 +0700

    add scripft file for running the game on windows

---
+++ run.bat
@@ -0,0 +1,26 @@
+@echo off
+:: Game Runner
+cd ./runner-publish/
+start "" dotnet GameRunner.dll
+:: Game Engine
+cd ../engine-publish/
+timeout /t 1
+start "" dotnet Engine.dll
+:: Game Logger
+cd ../logger-publish/
+timeout /t 1
+start "" dotnet Logger.dll
+:: Bots
+timeout /t 3
+cd ../reference-bot-publish/
+timeout /t 3
+:: Salah satu ReferenceBot nanti diganti ke java -jar path (path-nya disesuaikan dengan direktori bot yang telah di-build) untuk diuji coba
+start "" dotnet ReferenceBot.dll
+timeout /t 3
+start "" dotnet ReferenceBot.dll
+timeout /t 3
+start "" dotnet ReferenceBot.dll
+timeout /t 3
+start "" dotnet ReferenceBot.dll
+cd ../
+pause
